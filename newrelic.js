770772030ae07c13d21eb24107e558b52189ec28
Charles Anderson <charles.anderson.dev@gmail.com>
2018-10-08 09:06:18 -0700

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '70cede2b5bcb5c29337e8eec748142af7784ac04',
+  license_key: '',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
